14b0495ca7cea9e08b856a3958fe123bb9360cec
Walter26 <walterrafael26@gmail.com>
2020-05-20 12:21:32 -0600

    Add: begining of sourceCode

---
+++ Source Code/Arkanoid/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Arkanoid
+{
+    public partial class Form1 : Form
+    {
+        private int increment;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            increment = 10;
+            WindowState = FormWindowState.Maximized;
+            Height = Screen.PrimaryScreen.Bounds.Height;
+            Width = Screen.PrimaryScreen.Bounds.Width;
+        }
+
+        private void Form1_KeyPress(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    pictureBox1.Left -= increment;
+                    break;
+                case Keys.Right:
+                    pictureBox1.Left += increment;
+                    break;
+            }
+        }
+    }
+}
